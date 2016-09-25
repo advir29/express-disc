17a7e1e7581006acc1fa78fcb6b9f07118a6f701
td512 <td512@users.noreply.github.com>
2016-09-25 18:59:35 +1300

    Updated build script

--- build.sh
+++ build.sh
@@ -1,1 +1,13 @@
-dnf install pungi && pungi --nosource --nodebuginfo -G -C -B --flavor Amahi --name Amahi --ver 10 -c amahi.ks --isfinal && pungi --nosource --nodebuginfo -I --flavor Amahi --name Amahi --ver 10 -c amahi.ks --isfinal
+#!/bin/bash
+build()
+{
+if rpm -q pungi > /dev/null; 
+then 
+echo "Found pungi! Starting build..."; 
+pungi --nosource --nodebuginfo -G -C -B --flavor Amahi --name Amahi --ver 10 -c amahi.ks --isfinal && pungi --nosource --nodebuginfo -I --flavor Amahi --name Amahi --ver 10 -c amahi.ks --isfinal
+else
+dnf install pungi -y
+build
+fi
+}
+build
